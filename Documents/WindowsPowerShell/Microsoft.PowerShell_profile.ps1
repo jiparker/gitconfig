883944b6aadf81d4676313d35d6da59887184f20
cory.huebner <cory.huebner@asolutions.com>
2014-12-17 08:41:11 -0600

    Added ps profile

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+﻿
+# Load posh-git example profile
+. "$HOME\.poshgit-profile.ps1"
+
+function mklink {
+    cmd /c mklink $args[0] $args[1]
+}
+
+function runDeltas($deltaDirectory=".", $minDelta=0, $maxDelta=99999, $dbName="TMS") {
+	ls | 
+	where { $_ -match "(\d+)_.*" } | 
+	Select-Object Name, @{Name="DeltaNumber";Expression={ [int]($matches[1])}} | 
+	where { ($_.DeltaNumber -ge $minDelta) -and ($_.DeltaNumber -le $maxDelta) } | 
+	Sort-Object DeltaNumber | 
+	% {
+		Write-Host "Running $($_.Name)" -foregroundcolor "cyan";
+		$extension = (dir $_.Name | Select Extension);
+		echo $extension;
+		If($extension -eq ".ref")
+		{
+			Get-Content $_.Name | % { SQLCMD.EXE -i $_ -s localhost -v dbName $dbName };
+		}
+		Else
+		{
+			SQLCMD.EXE -i $_.Name -s localhost -v dbName=$dbName;
+		}
+	}	
+}
